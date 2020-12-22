f8b8e978c44f9f513ca6131694921c19b5e56f74
Agustinus Nathaniel <berserk.nate@gmail.com>
2020-12-22 15:45:24 +0700

    refactor: remove link

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -14,11 +14,9 @@ const Header = () => {
       textColor="white"
       background="linear-gradient(135deg, rgba(246,173,85,1) 0%, rgba(221,107,32,1) 100%);"
     >
-      <AccessibleLink href="/">
-        <Heading as="h1" display="flex" alignItems="center">
-          <Image src="/take-away.png" height={8} marginRight={2} /> YukMakan!
-        </Heading>
-      </AccessibleLink>
+      <Heading as="h1" display="flex" alignItems="center">
+        <Image src="/take-away.png" height={8} marginRight={2} /> YukMakan!
+      </Heading>
 
       <Box marginLeft="auto">
         <ThemeToggle />
